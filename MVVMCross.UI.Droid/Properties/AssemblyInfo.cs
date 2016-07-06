6a6b912c14736521f2366090a41f7932e57a642e
Damian Pumar <damianpumar@gmail.com>
2016-07-06 10:06:25 -0300

    Cambios en assembly

--- MVVMCross.UI.Droid/Properties/AssemblyInfo.cs
+++ MVVMCross.UI.Droid/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("FindApp.Droid")]
+[assembly: AssemblyTitle("MVVMCross.Core.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
